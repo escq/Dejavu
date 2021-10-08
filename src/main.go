51fbe31b897042bca38f39beb89ad314bf936a6c
escq <whuzz@protonmail.com>
2021-10-08 22:14:07 +0000

    Checking if the first input is similÃar to first word of the array

--- src/main.go
+++ src/main.go
@@ -4,18 +4,34 @@ import (
   "fmt"
   "os/exec"
   "os"
+  "strings"
+  "bufio"
+
+  term "github.com/nsf/termbox-go"
 )
 
-func main() {
-  const words = "Hello world\nHello world\nHello world"
-  clear()
-  fmt.Println(words)
+type cell struct {
+  char rune
+  fg  term.Attribute
+  bg  term.Attribute
 }
 
-func clear() {
-  cmdName := "clear"
-  cmd := exec.Command(cmdName)
-  cmd.Stdout = os.Stdout
-  cmd.Run()
+func main() {
 
+  words :=[4]string{"easy","peasy","lemon","squeezy"}
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Println("  " + words[0])
+
+  for {
+    fmt.Print("> ")
+    text, _ := reader.ReadString('\n')
+    text = strings.Replace(text, "\n", "", -1)
+
+    if strings.Compare(words[0], text) == 0 {
+      fmt.Println("Correct word");
+      break;
+    } else {
+      fmt.Println("Incorrect word")
+    }
+  }
 }
